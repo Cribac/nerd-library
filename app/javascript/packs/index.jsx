63fd216e3c74f88cae46631c2f611fcd3a6a7da9
Christian Bachhelm <cbachhelm@gmail.com>
2020-03-01 16:46:06 +0100

    Update ApolloProvider component.

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import { render } from 'react-dom';
 import Provider from '../components/Provider';
 
-render(<Provider>::foo::👻</Provider>, document.querySelector('#root'));
+render(<Provider>👻</Provider>, document.querySelector('#root'));
