b31e6784cc30e72bc85631b3b0dc1e4be3877301
Christian Bachhelm <cbachhelm@gmail.com>
2020-08-22 17:15:44 +0200

    Please rubocop.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 module Types
+  # User
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :email, String, null: false
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Item
 class Item < ApplicationRecord
   belongs_to :user
 end
